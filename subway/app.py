9ea09b5abeb73186b57e5af52e7c8c94f9b572d1
kaf97 <118801716+kaf97@users.noreply.github.com>
2022-12-14 14:36:04 +0900

    s

--- subway/app.py
+++ subway/app.py
@@ -43,8 +43,9 @@ st.write(df)
 # plt.pie(x=x, labels=labels, autopct='%.1f%%')
 # st.pyplot(fig5)
 
-fig = px.histogram(df, x='호선',title='호선별 이용자수' )
-st.plotly_chart(fig)
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['호선별 이용자수','5시30분 이용자수','6시30분 이용자수','7시30분 이용자수','8시30분 이용자수'])
+fig = px.histogram(df, x='호선')
+tab1.plotly_chart(fig)
 
 df.pivot_table(index='호선', columns='구분', values='5시30분', aggfunc='sum')
 fp = df.pivot_table(index='호선', columns='구분', values='5시30분', aggfunc='sum')
@@ -53,7 +54,22 @@ zp = fp.fillna(0)
 
 # df7 = px.data.df()
 fig7 = px.density_heatmap(df, x='호선', y="5시30분", marginal_x="rug", marginal_y="histogram")
-st.plotly_chart(fig7)
+tab2.plotly_chart(fig7)
 
+fig8 = px.density_heatmap(df, x='호선', y="6시30분", marginal_x="rug", marginal_y="histogram")
+tab3.plotly_chart(fig8)
 
-px.df(df_0233,	x='호선', y='5시30분', color='count',color_continuous_scale='Greens')
+fig9 = px.density_heatmap(df, x='호선', y="7시30분", marginal_x="rug", marginal_y="histogram")
+tab4.plotly_chart(fig9)
+
+fig0 = px.density_heatmap(df, x='호선', y="8시30분", marginal_x="rug", marginal_y="histogram")
+tab5.plotly_chart(fig0)
+
+palette_redwhite = ['#ffffff', '#ff0000']
+fig = px.density_heatmap(df, x = 'x', y = 'lane_num_shift', z = 'lanechange', histfunc = 'count',
+                         color_continuous_scale = palette_redwhite, range_color = [0, 30],
+                         nbinsx = 30)
+    
+fig.update_xaxes(range = [-100, 200])
+    
+fig.show()
